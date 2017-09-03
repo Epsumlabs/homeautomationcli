e08cc64da582ac4fa03714b8b72e8ecbfb06f835
Sai Prasad <saiprasad@hotmail.com>
2017-09-03 22:45:59 +0530

    added more setup info

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 
+long_description='''
+	This project is a command line utility tool, complimented with Epsumlabs Open Source IoT platform project.
+'''
 setup(
     name='homeautomationcli',
-    version='0.0.1_Beta',
+    version='0.0.2',
     packages=['homeautomationcli'],
     url='https://github.com/Epsumlabs/homeautomationcli',
     license='MIT',
@@ -15,5 +18,12 @@ setup(
           'requests'
       ],
     description='Epsum Labs Home Automation CLI',
+    long_description=long_description,
     keywords = ['cli', 'homeautomation', 'IoT'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5'
+    )
 )
